7b70eebd90ade4b0fc4e1af6a73d7b1066a0dc54
ramachandraa <Ramachandranyamagoud@gmail.com>
2024-10-29 21:33:37 +0530

    This is My first matlab program

---
+++ #BASIC SIGNAL.py
@@ -0,0 +1,11 @@
+#BASIC SIGNAL
+import numpy as np
+import matplotlib.pyplot as plt 
+x=np.arange(1,12,1)
+y=[1,2,3,4]
+x=[1,2,3,4]
+plt.xlabel('Independent variable')
+plt.ylabel('Dependent Variable')
+plt.title('Basic Signal')
+plt.plot(x,y)
+plt.show()
